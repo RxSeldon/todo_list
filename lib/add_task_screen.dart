7fe90ed1025564850c00e448a1dde680165dd255
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-10-16 22:20:34 +0800

    updated addTaskScreen class

--- lib/add_task_screen.dart
+++ lib/add_task_screen.dart
@@ -1,19 +1,74 @@
 import 'package:flutter/material.dart';
 
-
-class AddTaskScreen extends StatefulWidget{
+class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
 
   @override
   State<AddTaskScreen> createState() => _AddTaskScreenState();
-
 }
 
-class _AddTaskScreenState extends State<AddTaskScreen>{
+class _AddTaskScreenState extends State<AddTaskScreen> {
   final _formKey = GlobalKey<FormState>();
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
 
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  void _saveTask() {
+    if (_formKey.currentState!.validate()) {
+      print('Task Saved!');
+      print('Title: ${_titleController.text}');
+      print('Description: ${_descriptionController.text}');
+    }
+  }
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('New Task'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(),
+                ),
+                maxLength: 50,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Please entera title';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 3,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _saveTask,
+                child: const Text('Save Task'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
