6e705ccb06ae47f8587e58a3bde516a9aa8b2a53
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-10-17 20:15:14 +0800

    updated the home_screen class to stateful widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/models/task.dart';
 import 'add_task_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Task> _tasks = [];
+
+  Future<void> _navigateToAddTask() async {
+    final newTask = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddTaskScreen()),
+    );
+
+    if (newTask != null && newTask is Task) {
+      setState(() {
+        _tasks.add(newTask);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,19 +52,26 @@ class HomeScreen extends StatelessWidget {
           shadowColor: Colors.black26,
         ),
       ),
-      body: const Center(
-        child: Text(
-          'No tasks yet!',
-          style: TextStyle(fontSize: 18, color: Colors.grey),
-        ),
-      ),
+      body: _tasks.isEmpty
+          ? const Center(
+              child: Text(
+                'No tasks yet!',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return ListTile(
+                  leading: const Icon(Icons.check_box_outline_blank),
+                  title: Text(task.title),
+                  subtitle: Text(task.description ?? 'No description'),
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddTaskScreen()),
-          );
-        },
+        onPressed: _navigateToAddTask,
         child: const Icon(Icons.add),
       ),
     );
